6aac23cd96b004c748d74436c178c182f2f65ed0
Katherine Solis <ktssolis36@gmail.com>
2025-10-16 23:28:40 -0600

    Design correction (login)

--- pages/login.php
+++ pages/login.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Componente Bootstrap</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<link href="css/login.css" rel="stylesheet"/>
+<link href="../css/login.css" rel="stylesheet"/>
 </head>
 <body>
-    <section class="vh-100 gradient-custom">
+    <section class="vh-100">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
@@ -28,9 +28,7 @@
               <div data-mdb-input-init class="form-outline form-white mb-4">
                 <input type="password" id="typePasswordX" class="form-control form-control-lg" />
                 <label class="form-label" for="typePasswordX">Password</label>
-              </div>
-
-              <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+              </div>             
 
               <button data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
 
