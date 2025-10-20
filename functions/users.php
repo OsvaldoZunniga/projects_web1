2bd6fcc44a213108b1d499248e44bf88ec295fc5
Katherine Solis <ktssolis36@gmail.com>
2025-10-19 19:31:52 -0600

    Update and email functions

--- functions/users.php
+++ functions/users.php
@@ -1,5 +1,10 @@
 <?php 
-require_once '../database/connection.php';
+require_once '../database/connection.php'; 
+require '../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 
 $conn = getConnection_BD();
 
@@ -13,9 +18,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $contrasena = $_POST['contrasena'];
     $contrasena_confirm = $_POST['contrasena_confirm'];
     $idRoles = $_POST['idRoles'];
+    $token = '';
 
     if ($contrasena !== $contrasena_confirm) {
-        die("Las contraseñas no coinciden.");
+        header("Location: ../pages/signUpUsers.php?msg=pass_mismatch");
+        exit;
     }
 
     $contrasena_hashed = password_hash($contrasena, PASSWORD_DEFAULT);
@@ -31,20 +38,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (move_uploaded_file($_FILES['fotografia']['tmp_name'], $destino)) {
             $foto_ruta = 'assets/' . $nuevoNombre; 
         } else {
-            echo "No se pudo subir la imagen.";
+            echo "No se pudo subir la imagen.";//considerar cambiar por una alerta
         }
     }
 
+    $token = bin2hex(random_bytes(16));
 
     $sql = "INSERT INTO usuarios 
-        (nombre, apellido, cedula, nacimiento, correo, telefono, fotografia, contrasena, idRoles) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        (nombre, apellido, cedula, nacimiento, correo, telefono, fotografia, contrasena, idRoles, token) 
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssssssssi", $nombre, $apellido, $cedula, $nacimiento, $correo, $telefono, $foto_ruta, $contrasena_hashed, $idRoles);
+    $stmt->bind_param("ssssssssss", $nombre, $apellido, $cedula, $nacimiento, $correo, $telefono, $foto_ruta, $contrasena_hashed, $idRoles, $token);
 
+    //correos
     if ($stmt->execute()) {
-        echo "Usuario registrado correctamente.";
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'aventomescr@gmail.com';
+            $mail->Password = 'ubon jmov ryip sxmk'; 
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+            $mail->setFrom('aventomescr@gmail.com', 'AventonesCR');
+            $mail->addAddress($correo, $nombre);
+
+            $mail->isHTML(true);
+            $mail->Subject = 'Activar cuenta';
+            
+            $activationLink = "http://localhost/projects_web1/functions/activate.php?email=$correo&token=$token";
+            $mail->Body = "Hola $nombre,<br><br>Para activar tu cuenta, haz clic en el siguiente enlace:<br>
+                           <a href='$activationLink'>Activar cuenta</a><br><br>Gracias!";
+
+            $mail->send();
+           header("Location: ../pages/login.php?msg=pending");
+           exit();
+        } catch (Exception $e) {
+            header("Location: ../pages/login.php?msg=invalid");
+            exit();
+        }
     } else {
         echo "Error: " . $stmt->error;
     }
