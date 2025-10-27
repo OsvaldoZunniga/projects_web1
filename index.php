7937ff0311d3b19fe7cefd40f6c9306abe6e4429
OsvaldoZunniga <osvaldo66239@gmail.com>
2025-10-27 00:46:52 -0600

    identation fixed

--- index.php
+++ index.php
@@ -60,44 +60,44 @@ require_once 'database/connection.php';
     <?php endif; ?>
 
     <section class="vh-100">
-    <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card fondo text-white" style="border-radius: 1rem;">
-            <div class="card-body p-5 text-center">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card fondo text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-                <div class="mb-md-5 mt-md-4 pb-5">
+                            <div class="mb-md-5 mt-md-4 pb-5">
 
-                <h2 class="fw-bold mb-2 text-uppercase">Bienvenido</h2>
-                <p class="text-white-50 mb-5">Inicia sesión aquí!</p>
+                                <h2 class="fw-bold mb-2 text-uppercase">Bienvenido</h2>
+                                <p class="text-white-50 mb-5">Inicia sesión aquí!</p>
 
-                <form action="/functions/signIn.php" method="POST">
+                                <form action="/functions/signIn.php" method="POST">
 
-                    <div class="form-outline form-white mb-4">
-                    <input type="email" name="correo" id="typeEmailX" class="form-control form-control-lg" required/>
-                    <label class="form-label" for="typeEmailX">Email</label>
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="email" name="correo" id="typeEmailX" class="form-control form-control-lg" required/>
+                                        <label class="form-label" for="typeEmailX">Email</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="password" name="contrasena" id="typePasswordX" class="form-control form-control-lg" required/>
+                                        <label class="form-label" for="typePasswordX">Contraseña</label>
+                                    </div>             
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Iniciar Sesión</button>
+                                </form>
+
+                            </div>
+                            <div>
+                                <p class="mb-0">¿No tienes una cuenta? 
+                                    <a href="/pages/signUpUsers.php" class="text-white-50 fw-bold">Regístrate aquí</a>
+                                </p>
+                            </div>
+
+                        </div>
                     </div>
-
-                    <div class="form-outline form-white mb-4">
-                    <input type="password" name="contrasena" id="typePasswordX" class="form-control form-control-lg" required/>
-                    <label class="form-label" for="typePasswordX">Contraseña</label>
-                    </div>             
-
-                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Iniciar Sesión</button>
-                </form>
-
                 </div>
-                <div>
-                <p class="mb-0">¿No tienes una cuenta? 
-                    <a href="/pages/signUpUsers.php" class="text-white-50 fw-bold">Regístrate aquí</a>
-                </p>
-                </div>
-
-            </div>
             </div>
         </div>
-        </div>
-    </div>
     </section>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
